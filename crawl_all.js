1e0df68aa8600f8893e7444e10124f631d897654
Max Ma (RD-CN) <max_ma@trendmicro.com.cn>
2017-10-17 16:54:00 +0800

    console sorted result list

--- crawl_all.js
+++ crawl_all.js
@@ -7,9 +7,23 @@ function getVoteCount(pageNum){
 	var html = parser.parseFromString(http.responseText,'text/html')
 	var nickname = html.querySelector('.x1')
 	var like_count = html.querySelector('.like_btn').getAttribute('data-num')
-	console.log(nickname.innerText,':',like_count,'个赞')
+	if(like_count >= 10000){
+		console.log(nickname.innerText,':',like_count,'个赞')
+		return [nickname.innerText,like_count];
+	}else{
+		return ;
+	}
+
 }
 
-for (var i =250;i<310;i++){
-    getVoteCount(i)
-}
+var namelist = []
+for (var i =250;i<320;i++){
+	namelist.push(getVoteCount(i))
+    }
+var unsorted_list = namelist.filter(function(n){return n!= undefined})
+console.table(unsorted_list.sort(function(a,b){if(a[1] > b[1]){return -1} else {return 1}}))
+// getVoteCount(259)
+// getVoteCount(298)
+// getVoteCount(308)
+// getVoteCount(313)
+
---
+++ monitor.js
@@ -0,0 +1,71 @@
+function delete_cookie_and_reload(userId) {
+  // document.cookie = 'Hm_lvt_431673927acf89fbdf45f2181547bc09' + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  var expires = "";
+  var date = new Date();
+  date.setTime(date.getTime() + (24*60*60*1000));
+  expires = "; expires=" + date.toUTCString();
+  document.cookie = "info" + userId + "=0" + expires + "; path=/";
+}
+function vote(userId){
+	//var vote_btn = document.querySelector('.like_btn')
+	//vote_btn.click()
+	var http = new XMLHttpRequest()
+	http.open('GET','http://anniversary15.kaytrip.com/index.php/main/agreeYes?id='+userId,true)
+	http.send(null);
+	http.onreadystatechange = function() {
+        if (http.readyState != 4 || http.status != 200) {
+            return;
+        }
+
+        var data = {
+            msg: '解析失败'
+        }
+        try {
+            data = JSON.parse(http.responseText)
+        } catch (err) {
+        }
+
+        if (data.status == 1) {
+            console.log('=> 成功:' + data.msg + ' - ' + http.status + ' - ' + data.text)
+        } else {
+        	delete_cookie_and_reload()
+            console.error('=> 失败:' + data.msg + ' - ' + http.status + ' - ' + data.text)
+        }
+    }
+	
+}
+
+function vote_n(count,userId){
+	for(var i = 0;i<count;i++){
+		delete_cookie_and_reload(userId)
+		vote(userId)
+	}
+}
+
+function getVoteCount(pageNum){
+	var http = new XMLHttpRequest()
+	var url = 'http://anniversary15.kaytrip.com/index.php/main/info/'+pageNum 
+	http.open('GET',url,false)
+	http.send(null)
+	var parser = new DOMParser()
+	var html = parser.parseFromString(http.responseText,'text/html')
+	var nickname = html.querySelector('.x1')
+	var like_count = html.querySelector('.like_btn').getAttribute('data-num')
+	if(like_count >= 3000){
+		console.log(nickname.innerText,':',like_count,'个赞')
+		return like_count;
+	}
+	return;
+}
+// getVoteCount(308)
+
+function monitor(){
+	setInterval(function(){
+	if (getVoteCount(298)-getVoteCount(313) <= 10000){
+		console.log('目标正在接近我方，即将展开反击')
+	 	vote_n(500,298)
+	}
+},1000*60)
+}
+
+
